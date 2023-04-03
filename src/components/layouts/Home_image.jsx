45adf44a4454f6e5973f5be014e2d2249c3b3bdc
Thiago Oliveira <thiagooliveira745@gmail.com>
2023-04-02 21:48:23 -0300

    botão home_image

--- src/components/layouts/Home_image.jsx
+++ src/components/layouts/Home_image.jsx
@@ -4,6 +4,7 @@ import imagem from '../../assets/img/Personal finance.gif'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 function Home_image() {
 
@@ -12,6 +13,7 @@ function Home_image() {
             <Row>
                 <Col>
                     <h2 className={styles.text}>Transforme sua vida financeira de maneira fácil e inteligente com nosso site de finanças. Organize suas finanças pessoais e conquiste a tão sonhada <b>estabilidade financeira.</b></h2>
+                    <Link to='/calculadora'><button className={styles.button56}>Experimente agora!</button></Link>
                 </Col>
                 <Col>
                     <img src={imagem} className={styles.imagem}/>
